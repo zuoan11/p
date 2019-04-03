a70acf40a4f70bf8cd404a5087398c3e340e7ac3
renyu <renyu@donews.com>
2019-04-04 00:26:17 +0800

    初始化

---
+++ tools.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 格式化打印数据
+ * @Author   左岸
+ * @DateTime 2019-04-04
+ * @email    [renyu@donews.com]
+ * @param    [type]             $data [description]
+ * @return   [type]                   [description]
+ */
+if (!function_exists('p')) {
+	function p($data) {
+		echo '<pre>';
+		print_r($data);
+	}
+}
